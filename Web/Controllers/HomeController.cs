b12a0166f6b43a0a662e9eab679594aed44fd7da
Ponant <karim.murr@outlook.com>
2019-01-28 15:00:03 +0100

    Add project files.

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("httpClient");
+        }
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("api/values");
+            var content = await response.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject(content);
+            return new JsonResult(values);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View(); 
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
